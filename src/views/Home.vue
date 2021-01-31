ec6afce14f75334f23e6b207cd195f94488d6c9a
ahmetyasirkilic <ahmetyasirkilic@icloud.com>
2021-01-31 23:00:56 +0300

    first-commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="js">
+import { CBox, CButton, CGrid } from '@chakra-ui/vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+      CGrid, CBox
   }
 }
 </script>
